b28de54a070f21be907a88771994b138d1d5447f
JohnZhang888 <98006880+JohnZhang888@users.noreply.github.com>
2024-08-12 18:43:59 +0800

    Demo 1 (Hotfix 1)

--- scripts/level-check.js
+++ scripts/level-check.js
@@ -1,20 +1,20 @@
 const levelNum = 10
 
 if (Cookies.get('highestLevel') == undefined) {
-  Cookies.set('highestLevel', 0)
+  Cookies.set('highestLevel', 0, { expires: 30000 })
 }
 
 if (document.querySelector('html').dataset.num != undefined) {
-  
+
   if (Number(document.querySelector('html').dataset.num) != 1 && Number(Cookies.get('highestLevel')) + 1 < Number(document.querySelector('html').dataset.num)) {
     document.querySelector('.container').innerHTML = ''
     window.location.href = "404.html"
   } else {
     if (Number(document.querySelector('html').dataset.num) > Number(Cookies.get('highestLevel'))) {
-      Cookies.set('highestLevel', document.querySelector('html').dataset.num,  {sameSite: false})
+      Cookies.set('highestLevel', document.querySelector('html').dataset.num, { expires: 30000 })
     }
-    Cookies.set('recentLevel', document.querySelector('html').dataset.num, {sameSite: false})
-    const puzzleBox = document.querySelector('.puzzle-box') 
+    Cookies.set('recentLevel', document.querySelector('html').dataset.num, { expires: 30000 })
+    const puzzleBox = document.querySelector('.puzzle-box')
     puzzleBox.classList.remove('d-none')
   }
 }
